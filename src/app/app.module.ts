c4166fedcc535e4bd1ed1318a399e268b5662ee1
igor2000xp <igorring@gmail.com>
2022-12-23 00:38:55 +0100

    feat: add Angular Material and CDK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterialExampleModule } from './material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, MaterialExampleModule],
   providers: [],
   bootstrap: [AppComponent],
 })
